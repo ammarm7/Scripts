888cf109b681988f4031b99e9f9ae40b0839b02f
ammar <ammarm7@gmail.com>
2016-02-29 11:23:26 -0500

    Revert "Older version of android ss"
    This reverts commit 296af96fea9114ff97c232d97aa08fbe103e6059.

--- Android_Screenshot/android_screenshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-# echo -e "Name: \c"
-# read name
-adb shell screencap -p /name.png
-adb pull -p -a /sdcard/name.png
-mv name.png ~/Desktop
-adb shell rm /sdcard/name.png
-open /Users/ammarmohamed/Desktop/name.png 
